21fb48b9f27de07066592a8011894af8cf3d48f5
marattm <mtulepbayevmonnie@hawk.iit.edu>
2018-03-22 19:04:58 -0500

    added translation API in the ajax part of SpeechToText.js

--- app/ajax/speechToText.js
+++ app/ajax/speechToText.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var app = express();
+var unirest = require('unirest');
 
 app.get('/', function(req, res, next) {
+    
+    try {
     // translation function call
-    res.json(req.query.text)
+
+    
+
+    var query = req.query.text;
+    var targetLang = "fr";
+    var sourceLang = "en";
+    unirest.get(
+        "https://google-translate-proxy.herokuapp.com/api/translate?"
+        + "query=" + query
+        + "&targetLang=" + targetLang
+        + "&sourceLang=" + sourceLang)
+        .end(function (result) {
+            res.json(result.body.extract.translation);
+        });
+
+
+
+        
+        
+    } catch (error) {
+        
+        console.log("translation error");
+        console.error(error);
+        res.json("translation error");
+
+    }
+
 });
 
 module.exports = app;
